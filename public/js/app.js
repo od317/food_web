46f1260c32baba7f3555396dbae7af2e92f632ed
osama drbas <osamadrbas31@gmail.com>
2022-10-01 13:36:40 +0300

    new

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,14 @@ const health=`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=$
 const home_search_button=document.querySelector('.home-head-search-button-js');
 
 
+let breakfast = ["breakfast","Bacon","Pancakes","Avocado Toast","Breakfast Sandwich"];
+let dinner = ["dinner","taco","Pasta Napolitana","Chicken Potpie Casserole","Grilled Lemony Chicken and Kale"];
 
-fetchapi(`https://api.edamam.com/search?q=onion&app_id=${app_id}&app_key=${app_key}&to=50`);
+
+menu(breakfast);
+menu(dinner);
+
+fetchapi(`https://api.edamam.com/search?q=onion&app_id=${app_id}&app_key=${app_key}&to=50&imageSize=LARGE`);
 if(s!=null){
 s.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -59,3 +65,30 @@ h_b.forEach(e=>{
   fetchapi(`https://api.edamam.com/search?q=${searchq}&app_id=${app_id}&app_key=${app_key}&to=50&health=${e.innerText}
   `)
 })});
+
+
+
+async function menu(list){
+  let ht='';
+ for(let i=1;i<list.length;i++){ 
+let url = `https://api.edamam.com/search?q=${list[i]}&app_id=${app_id}&app_key=${app_key}&to=1`; 
+    let  response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+   let res = data.hits;
+   res.forEach(e=>{
+       let title = e.recipe.label;
+       title = title.split(' ');
+       title = title[0].split('-')[0]+" "+title[1].split('-')[0];
+       ht+=`<div class="card menu-card" >
+       <img src="${e.recipe.image}" class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">${title}</h5>
+         <a href="/recipe/${e.recipe.label}" class="btn btn-primary" id="b">View recipe</a>
+       </div>
+       </div>`; 
+   });
+  }
+  document.getElementById(list[0]).innerHTML=ht;
+}
+
