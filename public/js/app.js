535181281a3a334c6104224c6d5bb7ddc5f58442
osama drbas <osamadrbas31@gmail.com>
2022-10-03 15:15:53 +0300

    new

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,11 @@
 const app_id="88246266";
 const app_key="5c9709930f9269a830346c8b35d0e3ca";
 const s=document.querySelector("#search_form2");
+const searchfor = document.querySelector(".search-for-rec");
+const flitersnum = document.querySelector(".flitersnum");
 let searchq="";
 let Dish='';
-let diet='low-fat';
+let diet='';
 let Health='';
 let MealType='';
 const home_search_button=document.querySelector('.home-head-search-button-js');
@@ -23,7 +25,8 @@ s.addEventListener('submit',(e)=>{
 async function fetchapi(url){
   console.log(url);
   document.querySelector('.please_wait').innerHTML=`<div class="temp">Please Wait<ion-icon name="restaurant-outline"></ion-icon></div>`;
-   const response=await fetch(url);
+  searchfor.innerHTML=""; 
+  const response=await fetch(url);
    const data=await response.json();
    innerh(data.hits);
 }
@@ -76,8 +79,9 @@ document.addEventListener("click", e => {
 
 
 
+let flitersButtons= [{},{},{},{}];
 
-
+let flitersBut = document.querySelector('.flitersBut');
 
 let dish = document.querySelectorAll('.dish');
 
@@ -85,6 +89,10 @@ dish.forEach(e=>{
   e.addEventListener('click',()=>{
     Dish="dishType="+e.innerText;
     console.log(Dish);
+    flitersButtons[0]=[e.innerText,`<button class="${e.innerText}">${e.innerText}<span class="x">x</span></button>`];
+    addfliter();
+
+
   })
 })
 
@@ -94,7 +102,12 @@ health.forEach(e=>{
   e.addEventListener('click',()=>{
     Health="Health="+e.innerText;
     console.log(Health);
+    flitersButtons[1]=[e.innerText,`<button class="${e.innerText}">${e.innerText}<span class="x">x</span></button>`];
+    addfliter();
+
+  
   })
+  
 })
 
 
@@ -104,7 +117,12 @@ mealType.forEach(e=>{
   e.addEventListener('click',()=>{
      MealType="mealType="+e.innerText;
      console.log(MealType);
+     flitersButtons[2]=[e.innerText,`<button class="${e.innerText}">${e.innerText}<span class="x">x</span></button>`];
+     addfliter();
+
+   
   })
+
 })
 
 let Diet = document.querySelectorAll('.Diet');
@@ -113,9 +131,50 @@ Diet.forEach(e=>{
   e.addEventListener('click',()=>{
      diet="Diet="+e.innerText;
      console.log(diet);
+     flitersButtons[3]=[e.innerText,`<button class="${e.innerText}">${e.innerText}<span class="x">x</span></button>`];
+     addfliter();
   })
+  
+
 })
 
+function flitersfunc(but){
+  but.forEach(b=>{
+    if(b.length!=null){
+     document.querySelector(`.${b[0]}`).addEventListener('click',()=>{
+      for(let i=0;i<4;i++){
+      if(flitersButtons[i].length!=null&&flitersButtons[i][0]===b[0]){
+        flitersButtons[i]={};
+      }
+     }
+     console.log(flitersButtons); 
+     addfliter();
+    });
+    }
+  })
+  
+}
+
+function addfliter(){
+
+  flitersnum.innerHTML="";
+     flitersButtons.forEach(f=>{
+      
+      if(f.length!=null){
+      flitersnum.innerHTML+=f[1];
+      }
+    })
+    flitersfunc(flitersButtons);
+    flitersup();
+    console.log(searchq,'\n dish: '+Dish,'\n diet: '+diet,'\n health: '+Health,'\n mealtype: '+MealType)
+}
+
+function flitersup(){
+  Dish = flitersButtons[0].length ==null ? "": "dishType="+flitersButtons[0][0];
+  Health = flitersButtons[1].length ==null ? "": "Health="+flitersButtons[1][0];
+  MealType = flitersButtons[2].length ==null ? "": "mealType="+flitersButtons[2][0];
+  diet = flitersButtons[3].length ==null ? "": "Diet="+flitersButtons[3][0];
+}
 
 
 //fetchapi(`https://api.edamam.com/search?q=pizza&app_id=${app_id}&app_key=${app_key}&to=50&imageSize=LARGE`);
